7d6676bfb4cd96edb69d535f21fe458d462e419e
sgudur9 <18sgudur.stem@gmail.com>
2018-07-24 14:57:58 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -36,3 +36,9 @@ def lasso
   puts "lift right arm up and point right elbow diagonally to the right"
   puts "raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well"
 end
+
+#BUST-A-MOVE
+def bust_a_move
+  puts "be cool"
+  puts "be snazzy!"
+end
