3e3116dc91acd8f05579d1954857824cb86f7ee1
sgudur9 <18sgudur.stem@gmail.com>
2018-07-24 15:05:42 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -23,7 +23,7 @@ end
 def bob_the_reins
   puts "Holding your arms out in front of you,straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
-  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. Do this seven more times"
 end
 
 def lasso
