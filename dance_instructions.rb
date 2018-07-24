8a9d96c31ae10f1900306ebc0230be40153a2282
sgudur9 <18sgudur.stem@gmail.com>
2018-07-24 14:53:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,38 +1,37 @@
 
 # Code your methods below
+#LOWER BODY
+def starting_stance
+    puts "Plant legs far apart"
+end
 
-def lower_body
-    puts "Plant legs far apart, bend knees slightly and keep posture loose"
+def base_footwork
     puts "Lift right foot"
     puts "Return right foot to the ground"
     puts "Finishing with a small skip-step backward"
 end
-lower_body
 
+#SKIP-STEP
 def skip_step_left
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
   puts "Left, Right, Left and Left"
 end
-skip_step_left
 
 def skip_step_right
   puts "Lower right foot to the ground"
   puts "Bounce right back up slightly, kicking it a few inches back"
   puts "Right, Left, Right and Right"
 end
-skip_step_right
 
 def bob_the_reins
   puts "Holding your arms out in front of you,straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times"
 end
-bob_the_reins
 
 def lasso
   puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
   puts "lift right arm up and point right elbow diagonally to the right"
   puts "raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well"
 end
-lasso
