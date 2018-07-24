c7683ea86485be3ab9d8f1bcee2a6431021520cc
sgudur9 <18sgudur.stem@gmail.com>
2018-07-24 14:40:05 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,11 +1,11 @@
 
 # Code your methods below
 
-def Lower Body
+def lower body
     puts "Plant legs far apart, bend knees slightly and keep posture loose"
     puts "Lift right foot"
     puts "Return right foot to the ground"
     puts "Finishing with a small skip-step backward"
 end
+lower body
 
-Lower Body
